a4755a44695ead84381b7f701e2907c4e767dc17
Therlyson Ryan <132295029+Therlyson@users.noreply.github.com>
2024-04-28 23:31:09 -0300

    Adicionando a section contact e melhorando o código

--- app/app.ts
+++ app/app.ts
@@ -1,2 +1,182 @@
-const mobileNavBar = new MobileNavbar(".btn-menu", ".cabecalho_menu_paginas", ".cabecalho_menu_paginas a");
+// import Typed from 'typed.js';
+
+import { error } from "console";
+
+class MobileNavbar{ //classe para ajeitar a navegação responsiva da página
+    private mobileMenu: HTMLElement;
+    private navList: HTMLElement;
+    private navLinks: NodeListOf<HTMLAnchorElement>;
+    private activeClass: string;
+    constructor(mobileMenu: string, navList: string, navLinks: string){
+        this.mobileMenu = document.querySelector(mobileMenu) as HTMLElement;
+        this.navList = document.querySelector(navList) as HTMLElement;
+        this.navLinks = document.querySelectorAll(navLinks) as NodeListOf<HTMLAnchorElement>;
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
+        });
+    }
+
+    handleClick(){
+        this.animateLinks();
+        this.navList.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent(): void{
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+
+    init(): void{
+        if(this.mobileMenu){
+            this.addClickEvent();
+        }
+    }
+}
+
+
+class DestaqueHeader{ //classe para fixar enquanto o usuário está sobre uma página
+    private sections: NodeListOf<HTMLElement>;
+    private navList: HTMLElement;
+    private navLinks: NodeListOf<HTMLAnchorElement>;
+    
+    constructor(navList: string, sections: string, navLinks: string){
+        this.navList = document.querySelector(navList) as HTMLElement;
+        this.sections = document.querySelectorAll(sections) as NodeListOf<HTMLElement>;
+        this.navLinks = document.querySelectorAll(navLinks) as NodeListOf<HTMLAnchorElement>;
+    }
+
+    mudar(): void{
+        window.onscroll = () => {
+            this.sections.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop - 150;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+
+                if(top >= offset && top < offset + height){
+                    this.navLinks.forEach(links => {
+                        links.classList.remove('active');
+                        if(links.getAttribute('href') === '#' + id) {
+                            links.classList.add('active');
+                        }
+                    })
+                }
+            })
+        }
+    }
+
+    ativar(): void{
+        this.navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                this.navLinks.forEach(lnk => lnk.classList.remove('active'));
+                link.classList.add('active');
+            });
+        });
+    }
+}
+
+
+class FormSubmit {
+    private settings: { [key: string]: string };
+    private form: HTMLFormElement | null;
+    private formButton: HTMLElement | null;
+    private url: string | null;
+
+    constructor(settings: { [key: string]: string }) {
+        this.settings = settings;
+        this.form = document.querySelector(this.settings.form);
+        this.formButton = document.querySelector(this.settings.button);
+
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        } else {
+            this.url = null;
+        }
+
+        this.sendForm = this.sendForm.bind(this);
+    }
+
+    displaySuccess(): void{
+        if (this.form) {
+            this.form.innerHTML = this.settings.success;
+        }
+    }
+
+    displayError(): void{
+        if (this.form) {
+            this.form.innerHTML = this.settings.error;
+        }
+    }
+
+    getFormObject(): { [key: string]: string } {
+        const formObject: { [key: string]: string } = {};
+        const fields: NodeListOf<HTMLInputElement> = this.form.querySelectorAll("[name]");
+        fields.forEach((field: HTMLInputElement) => {
+            formObject[field.getAttribute("name")!] = field.value;
+        });
+        return formObject;
+    }
+
+    onSubmission(event: Event){
+        event.preventDefault();
+        const target = event.target as HTMLButtonElement;
+        target.disabled = true;   
+        target.innerText = "Enviando..."; 
+    }
+
+    async sendForm(event: Event){
+        try{
+            this.onSubmission(event);
+            await fetch(this.url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(this.getFormObject()),
+            });
+            this.displaySuccess();
+        }catch{
+            this.displayError();
+        }
+    }
+
+    init(){
+        if (this.form && this.formButton) {
+            this.formButton.addEventListener("click", () => this.sendForm);
+        }
+        return this;
+    }
+}
+
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possivel enviar sua mensagem!</h1>"
+});
+formSubmit.init();
+
+const mobileNavBar = new MobileNavbar("#btn-menu", ".cabecalho_menu_paginas", ".cabecalho_menu_paginas a");
 mobileNavBar.init();
+
+const destaqueHeader = new DestaqueHeader(".cabecalho_menu_paginas", "section", ".cabecalho_menu_paginas a");
+destaqueHeader.ativar(); 
+destaqueHeader.mudar();
+
+
+//deixar animação
+// const typed = new Typed(".home-generator", {
+//     strings: ["FullStack Development", "Software Engineer", "Scientific Researcher"],
+//     typeSpeed: 70,
+//     backSpeed: 70,
+//     backDelay: 1000,
+//     loop: true
+// });
---
+++ dist/js/app.js
@@ -0,0 +1,155 @@
+// import Typed from 'typed.js';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+    }
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+    handleClick() {
+        this.animateLinks();
+        this.navList.classList.toggle(this.activeClass);
+    }
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+    }
+}
+class DestaqueHeader {
+    constructor(navList, sections, navLinks) {
+        this.navList = document.querySelector(navList);
+        this.sections = document.querySelectorAll(sections);
+        this.navLinks = document.querySelectorAll(navLinks);
+    }
+    mudar() {
+        window.onscroll = () => {
+            this.sections.forEach(sec => {
+                let top = window.scrollY;
+                let offset = sec.offsetTop - 150;
+                let height = sec.offsetHeight;
+                let id = sec.getAttribute('id');
+                if (top >= offset && top < offset + height) {
+                    this.navLinks.forEach(links => {
+                        links.classList.remove('active');
+                        if (links.getAttribute('href') === '#' + id) {
+                            links.classList.add('active');
+                        }
+                    });
+                }
+            });
+        };
+    }
+    ativar() {
+        this.navLinks.forEach(link => {
+            link.addEventListener('click', () => {
+                this.navLinks.forEach(lnk => lnk.classList.remove('active'));
+                link.classList.add('active');
+            });
+        });
+    }
+}
+class FormSubmit {
+    constructor(settings) {
+        this.settings = settings;
+        this.form = document.querySelector(this.settings.form);
+        this.formButton = document.querySelector(this.settings.button);
+        if (this.form) {
+            this.url = this.form.getAttribute("action");
+        }
+        else {
+            this.url = null;
+        }
+        this.sendForm = this.sendForm.bind(this);
+    }
+    displaySuccess() {
+        if (this.form) {
+            this.form.innerHTML = this.settings.success;
+        }
+    }
+    displayError() {
+        if (this.form) {
+            this.form.innerHTML = this.settings.error;
+        }
+    }
+    getFormObject() {
+        const formObject = {};
+        const fields = this.form.querySelectorAll("[name]");
+        fields.forEach((field) => {
+            formObject[field.getAttribute("name")] = field.value;
+        });
+        return formObject;
+    }
+    onSubmission(event) {
+        event.preventDefault();
+        const target = event.target;
+        target.disabled = true;
+        target.innerText = "Enviando...";
+    }
+    sendForm(event) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                this.onSubmission(event);
+                yield fetch(this.url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
+                    },
+                    body: JSON.stringify(this.getFormObject()),
+                });
+                this.displaySuccess();
+            }
+            catch (_a) {
+                this.displayError();
+            }
+        });
+    }
+    init() {
+        if (this.form && this.formButton) {
+            this.formButton.addEventListener("click", () => this.sendForm);
+        }
+        return this;
+    }
+}
+const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Mensagem enviada!</h1>",
+    error: "<h1 class='error'>Não foi possivel enviar sua mensagem!</h1>"
+});
+formSubmit.init();
+const mobileNavBar = new MobileNavbar("#btn-menu", ".cabecalho_menu_paginas", ".cabecalho_menu_paginas a");
+mobileNavBar.init();
+const destaqueHeader = new DestaqueHeader(".cabecalho_menu_paginas", "section", ".cabecalho_menu_paginas a");
+destaqueHeader.ativar();
+destaqueHeader.mudar();
+export {};
+//deixar animação
+// const typed = new Typed(".home-generator", {
+//     strings: ["FullStack Development", "Software Engineer", "Scientific Researcher"],
+//     typeSpeed: 70,
+//     backSpeed: 70,
+//     backDelay: 1000,
+//     loop: true
+// });
